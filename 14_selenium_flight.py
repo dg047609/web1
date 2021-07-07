7c421563bc50e809b1de29a76d0d0fa11c6f7f6b
Steve Kim <dg047609@gmail.com>
2021-07-08 05:02:59 +0900

    selenium을 이용한 네이버 항공권 1-1, 1-2

--- 14_selenium_flight.py
+++ 14_selenium_flight.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
-browser = webdriver.Chrome("./chromedriver")
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+browser = webdriver.Chrome("./chromedriver.exe")
 browser.maximize_window() #창 최대화
 
 url = "https://flight.naver.com/flights/"
@@ -18,7 +23,14 @@ browser.find_element_by_xpath("//*[@id='recommendationList']/ul/li[1]/div/span")
 #항공권 검색 클릭
 browser.find_element_by_link_text("항공권 검색").click()
 
+try:
+    elem = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='content']/div[2]/div/div[4]/ul/li[1]")))
+    #성공했을 때 동작
+    print(elem.text) #첫 번째 결과 출력
+finally:
+    browser.quit()
+
 #첫번째 결과 출력
-elem = browser.find_element_by_xpath("//*[@id='content']/div[2]/div/div[4]/ul/li[1]")
-print(elem.text)
+# elem = browser.find_element_by_xpath("//*[@id='content']/div[2]/div/div[4]/ul/li[1]")
+# print(elem.text)
 
