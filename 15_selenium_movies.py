de9a461268e367d9bd51d02c552c91196b03dcd6
Steve Kim <dg047609@gmail.com>
2021-07-11 01:31:47 +0900

    selenium을 이용한 google movie 2-1

--- 15_selenium_movies.py
+++ 15_selenium_movies.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://play.google.com/store/movies/top?hl=ko&gl=US"
+url = "https://play.google.com/store/movies/top"
 headers = {
-    "User-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36",
     "Accept-Language":"ko-KR,ko"
     }
 
@@ -12,8 +12,13 @@ res.raise_for_status()
 
 soup = BeautifulSoup(res.text, "lxml")
 
-movies = soup.find_all("div", attrs={"class":"WHE7ib mpg5gc"})
+movies = soup.find_all("div", attrs={"ImZGtf mpg5gc"})
 print(len(movies))
 
-with open("movies.html", "w", encoding="utf8") as f:
-    f.write(soup.prettify()) #html 문서를 예쁘게 출력
+# with open("movies.html", "w", encoding="utf8") as f:
+#     f.write(soup.prettify()) #html 문서를 예쁘게 출력
+
+for movie in movies:
+    title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    print(title)
+
